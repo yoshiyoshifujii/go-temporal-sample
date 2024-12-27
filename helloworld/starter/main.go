9405cd3a9dc86f272630d8825fb2f382c4a1aabe
FUJII Yoshitaka <yoshiyoshifujii@gmail.com>
2024-12-27 15:50:54 +0900

    add helooworld starter

---
+++ helloworld/starter/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"github.com/yoshiyoshifujii/go-temporal-sample/helloworld"
+	"go.temporal.io/sdk/client"
+	"log"
+)
+
+func main() {
+	c, err := client.Dial(client.Options{})
+	if err != nil {
+		log.Fatalln("Unable to create client", err)
+	}
+	defer c.Close()
+
+	workflowOptions := client.StartWorkflowOptions{
+		ID:        "hello_world_workflowID",
+		TaskQueue: "hello-world",
+	}
+
+	we, err := c.ExecuteWorkflow(context.Background(), workflowOptions, helloworld.Workflow, "Temporal")
+	if err != nil {
+		log.Fatalln("Unable to execute workflow", err)
+	}
+
+	log.Println("Started workflow", "WorkflowID", we.GetID(), "RunID", we.GetRunID())
+
+	// Synchronously wait for the workflow completion.
+	var result string
+	err = we.Get(context.Background(), &result)
+	if err != nil {
+		log.Fatalln("Unable get workflow resultr", err)
+	}
+	log.Println("Workflow result:", result)
+}
